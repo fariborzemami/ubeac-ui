24ea56c743cf0e09a5f269894c901259221a8a44
fariborzemami <fariborz.emami1997@gmail.com>
2021-08-07 02:30:11 +0430

    add date picker component

---
+++ src/components/datePicker.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    v-if="visible"
+    >
+    <!--<u-persian-datetime-picker-->
+      <!--v-model="date"-->
+      <!-->-->
+    <!--</u-persian-datetime-picker>-->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    value: {
+      type: [Date, String]
+    },
+    format: {
+      type: String,
+      default: 'yyyyMMdd'
+    },
+    placeholder: {
+      type: String
+    },
+    firstDayOfWeek: {
+      type: Number,
+      default: 6
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    isRequired: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String
+    },
+    visible: {
+      type: Boolean,
+      default: true
+    },
+  },
+  date () {
+    return {
+      date: ''
+    }
+  },
+  methods: {
+    onLoad () {},
+    onClose () {},
+    opened () {},
+    select () {},
+    formatter () {}
+  }
+}
+</script>
+
---
+++ src/pages/components/datePicker.vue
@@ -0,0 +1,85 @@
+<route lang="yaml">
+name: Alert
+meta:
+  layout: full
+</route>
+
+<template>
+  <h1>Alert</h1>
+  <u-tabs>
+    <u-tab
+      :active="true"
+      title="options">
+      <p 
+      class="mt-3 text-start document-section">
+      <markdown></markdown>
+      </p>
+    </u-tab>
+    <u-tab
+      title="playground">
+      <u-live 
+      :code="code">
+      </u-live>
+    </u-tab>
+    <u-tab
+      title="examples">
+      <div class="row">
+        <div class="col">
+          <h4>Normal View</h4>
+          <template
+            v-for="v in alertVariants">
+            <u-alert
+              :variant="v"
+              @click="doSomthing()"
+              text="Alert text will be shown!"
+              >
+            </u-alert>
+              <div class="my-3"></div>
+          </template>
+        </div>
+        <div class="col">
+          <h4>Closable</h4>
+          <template
+            v-for="v in alertVariants">
+            <u-alert
+              :variant="v"
+              text="This is a closable alert!"
+              :closable="true"
+              >
+            </u-alert>
+              <div class="my-3"></div>
+          </template>
+        </div>
+      </div>
+    </u-tab>
+  </u-tabs>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { VueComponent } from '../../../docs/components/primitive/alert.md'
+const markdown = VueComponent
+const code = reactive(
+`
+<u-alert
+  variant="success"
+  @click="doSomthing()"
+  text="this is an alert!"
+  closable
+  >
+</u-alert>
+`
+)
+
+const doSomthing = () => {alert("Somthing");}
+const alertVariants = [
+        'primary', 
+        'secondary', 
+        'success', 
+        'danger', 
+        'warning', 
+        'info', 
+        'light',
+        'dark'
+      ]
+</script>
