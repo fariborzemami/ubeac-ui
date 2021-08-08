25c2ae06d7993bb01c4382b0e9ae688cc029e781
reza babakhani <babakhani.reza@gmail.com>
2021-08-09 00:45:34 +0430

    add i18n expersion loader

--- src/pages/loader_test/index.vue
+++ src/pages/loader_test/index.vue
@@ -4,11 +4,23 @@ meta:
 </route>
 
 <template>
-  <h1>Loader Test</h1>
+  <h1>
+    Loader Test
+  </h1>
+  <h2>
+    Change mode to v-model example
+  </h2>
   <pre>{{ modelExample }}</pre>
   <u-input 
     model="modelExample">
   </u-input>
+  <h2>
+    Change i18n expersion
+  </h2>
+  <span>
+    __application-name__
+    __application-name__
+  </span>
 </template>
 
 <script>
