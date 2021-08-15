e12384ac862d8c6f204ee5bb1f2b5b9191da0e03
Fariborz Emami <fariborz.emami1997@gmail.com>
2021-08-15 15:37:44 +0430

    add form component and form example page

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,7 +1,65 @@
 <template>
-  <slot />
+  <form 
+    @submit.stop.prevent="onSubmit">
+    <div
+      class="row mb-3">
+        <div
+          :class="`col-${cols}`"> 
+          <slot />
+        </div>
+    </div>
+  </form>
 </template>
 
-<script setup>
-// NOTE: Zero version, created because of need to use in demo pages
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String
+    },
+    cols: {
+      type: Number,
+      default: 4
+    },
+    confirm: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    },
+    autosave: {
+      type: Boolean,
+      default: false
+    },
+    autocomplete: {
+      type: Boolean,
+      default: false
+    },
+    layout: {
+      type: String,
+      default: 'horizental'
+    }
+  },
+  created () {
+    window.onload = this.onLoad()
+  },
+  data () {
+    FormData: this.modelValue
+  },
+  methods: {
+    onLoad (e) {
+      this.$emit('onLoad', e)
+    }
+  }
+}
 </script>
---
+++ src/pages/components/form.vue
@@ -0,0 +1,93 @@
+<route lang="yaml">
+name: Form
+meta:
+  layout: full
+</route>
+
+<template>
+  <DemoHeader
+    edit-link="https://github.com/ubeac/ubeac-ui/blob/main/docs/components/form/form.new.md"
+    title="Form"
+  />
+  <u-tabs>
+    <u-tab
+      :active="true"
+      title="API"
+    >
+      <p 
+        class="mt-3 text-start document-section"
+      >
+        <markdown />
+      </p>
+    </u-tab>
+    <u-tab
+      title="Playground"
+    >
+      <u-live 
+        :code="code"
+      />
+    </u-tab>
+    <u-tab
+      title="Examples"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h4>Default</h4>
+              <u-form></u-form>
+            <div class="my-3" />
+          </div>
+          <div class="col">
+              <u-form></u-form>
+            <div class="my-3" />
+          </div>
+          <div class="col">
+            <h4>Disabled</h4>
+              <u-form></u-form>
+            <div class="my-3" />
+          </div>
+        </div>
+      </div>
+    </u-tab>
+  </u-tabs>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+import { VueComponent } from '../../../docs/components/form/form.new.md'
+const markdown = VueComponent
+const code = reactive(
+`<template>
+  <u-form />
+</template>
+
+<script>
+export default {
+   data () {
+     return {
+        
+     }
+   }
+ }
+<\/script>`
+)
+const examples = [
+  {
+    text: 'foo', 
+    disabled: false
+  },
+  {
+    text: 'bar', 
+    disabled: true
+  },
+  {
+    text: 'baz',
+    disabled: false
+  }
+      ]
+const doSomthing = () => {alert("Somthing");}
+
+const selectedRadioExample1 = ref('')
+const selectedRadioExample2 = ref('bar')
+const selectedRadioExample3 = ref('')
+</script>
